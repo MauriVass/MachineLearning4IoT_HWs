6ffd9fc09915986baafa6342b2c08647189997d3
MauriVass <maurizio140333@gmail.com>
2020-11-07 15:26:07 +0100

    Added HM1 files

---
+++ HM1/Ex1.py
@@ -0,0 +1,47 @@
+import tensorflow as tf
+import os
+
+input_path = 'raw_data/'
+output_file = 'tfrecord'
+
+#Read all files in the input dir
+input_files = os.listdir(input_path)
+#Get .csv file
+csv_file = [f for f in input_files if f.find('.csv')>0][0]
+#Open .csv file
+file = open(input_path+csv_file,'r')
+#This variable is used to 'test' the performance of the tfrecord
+#Set the initial size equal to the .csv file
+initial_size =  os.path.getsize(input_path+csv_file)
+
+with tf.io.TFRecordWriter(output_file) as writer:
+	for f in file:
+		content = f.rstrip().split(',')
+		#Read a line. Format: date,time,temp,humi,wav_file
+		datetime = 14099898997
+		temperature = int(content[2])
+		humidity = int(content[3])
+		audio_file = content[4]
+		#Read .wav file
+		audio = tf.io.read_file(input_path+audio_file)
+		audio = audio.numpy()
+		#Add the .wav size
+		initial_size += os.path.getsize(input_path+audio_file)
+
+		#print(type(datetime),type(temperature),type(humidity),type(audio))
+
+		date_feature = tf.train.Feature(int64_list=tf.train.Int64List(value=[datetime]))
+		temp_feature = tf.train.Feature(int64_list=tf.train.Int64List(value=[temperature]))
+		humi_feature = tf.train.Feature(int64_list=tf.train.Int64List(value=[humidity]))
+		audio_feature = tf.train.Feature(bytes_list=tf.train.BytesList(value=[audio]))
+
+		mapping = {'datetime': date_feature,
+			'temperature': temp_feature,
+			'humidity': humi_feature,
+			'audio': audio_feature}
+		example = tf.train.Example(features=tf.train.Features(feature=mapping))
+
+		writer.write(example.SerializeToString())
+
+file_size = os.path.getsize(output_file)
+print(f'Initial size {initial_size}, final size {file_size} Ratio {(file_size/initial_size):.4f}')
---
+++ HM1/TFRecordReader.py
@@ -0,0 +1,37 @@
+import tensorflow as tf
+import wave
+import pyaudio
+
+def parser_function(example):
+	return tf.io.parse_single_example(example, mapping)
+
+input_file = 'tfrecord'
+raw_data = tf.data.TFRecordDataset(input_file)
+print(raw_data)
+
+mapping = {'datetime': tf.io.FixedLenFeature([], tf.int64, default_value=0),
+            'temperature':  tf.io.FixedLenFeature([], tf.int64, default_value=0),
+            'humidity':  tf.io.FixedLenFeature([], tf.int64, default_value=0),
+            'audio':  tf.io.FixedLenFeature([], tf.string, default_value='')}
+
+parsed_dataset = raw_data.map(parser_function)
+i = 0
+for p in parsed_dataset:
+	date = p['datetime'].numpy()
+	temp = p['temperature'].numpy()
+	hum = p['humidity'].numpy()
+	audio = p['audio'].numpy()
+	#print(audio)
+
+	waveFile = wave.open(f'audio{i}.wav','wb')
+	i+=1
+	waveFile.setnchannels(1)
+	a = pyaudio.PyAudio()
+	waveFile.setsampwidth(a.get_sample_size(pyaudio.paInt16))
+	waveFile.setframerate(48000)
+	waveFile.writeframes(audio)
+	waveFile.close()
+
+	entry = f'{date},{temp},{hum},{type(audio)}'
+	print(entry)
+
